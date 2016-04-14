452d9ca179350eb98af431af1da7720142fcac4c
Adnan Ali <adnanlone13@gmail.com>
2016-04-14 12:56:28 -0400

    addd message to browser Ui

--- public/js/app.js
+++ public/js/app.js
@@ -10,6 +10,8 @@ socket.on('connect', function () {
 socket.on('message', function (message) {
     console.log('New Message is:');
     console.log(message.text);
+    jQuery('.messages').append('<p>'+ message.text +'</p>');
+
 });
 
 //Handle submitting of new message
--- server.js
+++ server.js
@@ -13,7 +13,9 @@ io.on('connection', function (socket) {
 
     socket.on('message', function (message) {
         console.log('Message received ' + message.text);
-        socket.broadcast.emit('message', message);
+        //socket.broadcast is for sending message to everyone except the sender
+        io.emit('message', message);
+
     });
 
 
